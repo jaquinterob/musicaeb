a15a756b40a457e521b4b963d639e3ac2d9df10e
John Alexander Quintero <quinteroberrio@gmail.com>
2018-11-05 22:10:23 -0500

    cambio de title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es" dir="ltr">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <title>Logueo MEB</title>
+  <title>Inicio MEB</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <!-- Compiled and minified CSS -->
