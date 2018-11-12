3e07fc83e93b093cf9efcad0040d397694290756
John Alexander Quintero <quinteroberrio@gmail.com>
2018-11-11 20:46:41 -0500

    configuracion correcta de .gitignore

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,9 +1,14 @@
 <?php $host="localhost";
+# @Date:   2018-10-14T21:41:21-05:00
+# @Last modified time: 2018-10-14T21:49:44-05:00
+
+#comentario de prueba
+
+
 $user="root";
-$pass="";
-$db="2111145_prueba";
+ $pass="";
+  $db="musicaeb";
 $connect= new mysqli($host,$user,$pass,$db) or die ("error" .
 mysqli_errno($connect)); if (!mysqli_set_charset($connect, "utf8")) {
-    printf("Error cargando el conjunto de caracteres utf8: %s\n",
-    mysqli_error($connect)); exit(); }
-    ?>
+printf("Error cargando el conjunto de caracteres utf8: %s\n",
+mysqli_error($connect)); exit(); } ?>
